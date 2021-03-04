ffde9cc35f934bafb7a194bfd99abb04dadcbc52
MiracleWorker <dpalstn91@gmail.com>
2021-03-04 14:28:25 +0900

    Add string rotation

---
+++ 1.9 문자열 회전/1.9 문자열 회전/1.9 문자열 회전.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsSubstring(string str1, string str2);
+
+int main()
+{
+    string str1 = "erbottlewat";
+    string str2 = "waterbottle";
+
+    cout << IsSubstring(str1, str2);
+
+    return 0;
+}
+
+// x = erbottle
+// y = wat
+// str2 = yx
+// xyxy = erbottlewaterbottlewat
+bool IsSubstring(string str1, string str2)
+{
+    string xyxy = str1 + str1;
+    return xyxy.find(str2) != string::npos;
+}
