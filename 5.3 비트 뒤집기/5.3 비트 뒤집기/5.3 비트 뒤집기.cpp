3e04894e96ca0ccdf4cc5d90f0994af72cb76173
MiracleWorker <dpalstn91@gmail.com>
2021-03-08 16:59:22 +0900

    Add 5.4 next number

---
+++ 5.3 비트 뒤집기/5.3 비트 뒤집기/5.3 비트 뒤집기.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int input = 1775;
+	int count = 0;
+	int max = 0;
+	bool hasOne = true;
+
+	while (input / 2 != 1)
+	{
+		int remainder = input % 2;		
+
+		if (remainder == 1)
+		{
+			count++;
+		}
+		else
+		{
+			if (hasOne)
+			{
+				count++;
+				hasOne = false;
+			}
+			else
+			{
+				if (count > max)
+				{
+					max = count;
+				}
+				count = 0;
+			}
+		}
+
+		input /= 2;
+	}
+
+	cout << max << endl;
+
+	return 0;
+}
---
+++ 5.4 다음 숫자/5.4 다음 숫자/5.4 다음 숫자.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+// 양의 정수가 하나 주어졌다. 이 숫자를 2진수로 표기했을때 1비트의 개수가 같은 숫자중에서 다음으로 큰수와 다음으로 작은수를 구하라.
+
+int GetNextNumber(int value)
+{
+	int c = value;
+	int c0 = 0;
+	int c1 = 0;
+
+	if (c % 2 == 0)
+	{
+		while (c % 2 == 0)
+		{
+			c0++;
+			c >>= 1;
+		}
+
+		while (c % 2 == 1)
+		{
+			c1++;
+			c >>= 1;
+		}
+	}
+	else
+	{
+		while (c % 2 == 1)
+		{
+			c1++;
+			c >>= 1;
+		}
+	}	
+
+	int p = c1 + c0;
+
+	value = value | (1 << p);
+	value = value & ~((1 << p) - 1);
+	value = value | ((1 << (c1 - 1)) - 1);
+
+	return value;
+}
+
+int GetPrevNumber(int value)
+{
+	int c = value;
+	int c0 = 0;
+	int c1 = 0;
+
+	if (c % 2 == 0)
+	{
+		while (c % 2 == 0)
+		{
+			c0++;
+			c >>= 1;
+		}
+	}
+	else
+	{
+		while (c % 2 == 1)
+		{
+			c1++;
+			c >>= 1;
+		}
+
+		while (c % 2 == 0)
+		{
+			c0++;
+			c >>= 1;
+		}
+	}	
+
+	int p = c1 + c0;
+	value = value & ~((1 << p + 1) - 1);	
+	value = value | (((1 << c1 + 1) - 1) << (c1 + 1));
+
+	return value;
+}
+
+int main()
+{
+	int input = 13948; // 11011001111100
+	//next : 11011010001111 : 13967
+	//prev : 11011001111010 : 13946
+	cout << GetNextNumber(input) << endl;
+	cout << GetPrevNumber(input) << endl;
+	return 0;
+}
