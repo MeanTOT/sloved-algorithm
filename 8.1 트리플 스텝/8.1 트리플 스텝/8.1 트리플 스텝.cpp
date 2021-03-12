221de61846366288e1b569ba546f2f9bfe82b95d
MiracleWorker <dpalstn91@gmail.com>
2021-03-12 14:10:19 +0900

    Add new algorithms

---
+++ 8.1 트리플 스텝/8.1 트리플 스텝/8.1 트리플 스텝.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+void OneStep(int remainedStair);
+void TwoStep(int remainedStair);
+void ThreeStep(int remainedStair);
+
+int WayCount = 0;
+
+int main()
+{
+	int Stair = 5;
+	
+	OneStep(5);
+	TwoStep(5);
+	ThreeStep(5);
+
+	std::cout << WayCount;
+
+	return 0;
+}
+
+void OneStep(int remainedStair)
+{
+	if (remainedStair - 1 == 0)
+	{
+		WayCount++;
+		return;
+	}
+	else if (remainedStair - 1 < 0)
+	{
+		return;
+	}
+	else
+	{
+		OneStep(remainedStair - 1);
+		TwoStep(remainedStair - 1);
+		ThreeStep(remainedStair - 1);
+	}
+}
+
+void TwoStep(int remainedStair)
+{
+	if (remainedStair - 2 == 0)
+	{
+		WayCount++;
+		return;
+	}
+	else if (remainedStair - 2 < 0)
+	{
+		return;
+	}
+	else
+	{
+		OneStep(remainedStair - 2);
+		TwoStep(remainedStair - 2);
+		ThreeStep(remainedStair - 2);
+	}
+}
+
+void ThreeStep(int remainedStair)
+{
+	if (remainedStair - 3 == 0)
+	{
+		WayCount++;
+		return;
+	}
+	else if (remainedStair - 3 < 0)
+	{
+		return;
+	}
+	else
+	{
+		OneStep(remainedStair - 3);
+		TwoStep(remainedStair - 3);
+		ThreeStep(remainedStair - 3);
+	}
+}
---
+++ 8.3 마술 인덱스/8.3 마술 인덱스/8.3 마술 인덱스.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int FindMagicIndex(int arr[], int searchIndex, int size)
+{
+    if (arr[searchIndex] < 0 || arr[searchIndex] < searchIndex)
+    {
+        if (size - searchIndex == 1)
+            return -50000;
+        else
+            return FindMagicIndex(arr, (searchIndex + size) / 2, size);
+    }
+    else if (arr[searchIndex] > searchIndex)
+    {
+        return FindMagicIndex(arr, (searchIndex) / 2, size);
+    }
+    else if (arr[searchIndex] == searchIndex)
+    {
+        return searchIndex;
+    }        
+}
+
+int main()
+{
+    int A[6] = { -5, -5, -5, 3, 5, 6};
+
+    cout << FindMagicIndex(A, 6 / 2, 6);
+
+    return 0;
+}
