ae2a05a5b42a66a7c948bed849d629a306e5cfa8
Miracle Worker <dpalstn91@gmail.com>
2021-03-19 15:50:28 +0900

    Add new algorithm

---
+++ 프로그래머스 - 다리를 지나는 트럭/프로그래머스 - 다리를 지나는 트럭/프로그래머스 - 다리를 지나는 트럭.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Truck
+{
+    int weight;
+    int onBridgeTime;
+};
+
+void CheckCollapsedTimeTrucksOnBridge(queue<Truck>& trucksOnBridge, int currentTime, int bridgeLength, int& weightAmountOnBridge)
+{
+    if (trucksOnBridge.empty())
+        return;
+
+    if (currentTime - trucksOnBridge.front().onBridgeTime == bridgeLength)
+    {
+        weightAmountOnBridge -= trucksOnBridge.front().weight;
+        trucksOnBridge.pop();
+    }
+}
+
+void TryAddTruckToBridge(vector<int>& truck_weights, queue<Truck>& trucksOnBridge, int& weightAmountTrucksOnBridge, int weight, int currentTime)
+{
+    if (truck_weights.size() == 0)
+        return;
+
+    int nextTruckWeight = truck_weights.front();
+    if (weightAmountTrucksOnBridge + nextTruckWeight <= weight)
+    {
+        Truck truck{ nextTruckWeight, currentTime };
+        trucksOnBridge.push(truck);
+        weightAmountTrucksOnBridge += nextTruckWeight;
+        truck_weights.erase(truck_weights.begin());
+    }
+}
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) 
+{
+    int answer = 0;
+    queue<Truck> trucksOnBridge;
+
+    int weightAmountTrucksOnBridge = 0;
+    while (true)
+    {
+        if (truck_weights.size() == 0 && trucksOnBridge.size() == 0)
+            break;      
+
+        answer++;
+        CheckCollapsedTimeTrucksOnBridge(trucksOnBridge, answer, bridge_length, weightAmountTrucksOnBridge);   
+        TryAddTruckToBridge(truck_weights, trucksOnBridge, weightAmountTrucksOnBridge, weight, answer);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> truckWeights = { 7,4,5,6 };
+    cout << solution(2, 10, truckWeights);
+
+    return 0;
+}
---
+++ 프로그래머스 - 베스트앨범/프로그래머스 - 베스트앨범/프로그래머스 - 베스트앨범.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>  
+#include <algorithm>
+
+using namespace std;
+
+struct SongData
+{    
+    int index;
+    int playCount;
+};
+
+bool cmp(SongData data1, SongData data2)
+{
+    if (data1.playCount == data2.playCount)
+        return data1.index < data2.index;
+    return data1.playCount > data2.playCount;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) 
+{
+    vector<int> answer;
+    map<string, vector<SongData>> AllSongs;
+    map<int, vector<SongData>, greater<int>> AllSongsOrderd;
+    for (int i = 0; i < genres.size(); i++)
+    {
+        SongData data{ i, plays[i] };
+        AllSongs[genres[i]].push_back(data);
+    }
+
+    for (auto genre : AllSongs)
+    {
+        int PlayAmount = 0;
+        for (int i = 0; i < genre.second.size(); i++)
+        {
+            PlayAmount += genre.second[i].playCount;
+        }
+        AllSongsOrderd[PlayAmount] = genre.second;
+        sort(AllSongsOrderd[PlayAmount].begin(), AllSongsOrderd[PlayAmount].end(), cmp);
+    }
+
+    for (auto genre : AllSongsOrderd)
+    {
+        if (genre.second.size() > 1)
+        {
+            answer.push_back(genre.second[0].index);
+            answer.push_back(genre.second[1].index);
+        }   
+        else if (genre.second.size() == 1)
+        {
+            answer.push_back(genre.second[0].index);
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> genres = { "classic", "pop", "classic", "classic", "pop" };
+    vector<int> plays = { 500, 600, 150, 800, 2500 };
+
+    solution(genres, plays);
+
+    return 0;
+}
