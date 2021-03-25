8a5c51e7284b5934ebafb1d1eec7604f9961f4e6
Miracle Worker <dpalstn91@gmail.com>
2021-03-25 19:09:05 +0900

    Add new algorithm

---
+++ 프로그래머스 - 구명보트/프로그래머스 - 구명보트/프로그래머스 - 구명보트.cpp
@@ -0,0 +1,45 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) 
+{
+    int answer = 0;
+    sort(people.begin(), people.end());
+        
+    int startJindex = people.size() - 1;
+    for (int i = 0; i < people.size(); i++)
+    {
+        for (int j = startJindex; j >= 0; j--)
+        {
+            if (j <= i)
+            {
+                answer += startJindex - j + 1;
+                return answer;
+            }
+
+            if (people[i] + people[j] <= limit)
+            {
+                answer += startJindex - j + 1;
+                startJindex = j - 1;
+
+                if (j == i + 1)
+                    return answer;
+                else
+                    break;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> people = { 70, 50, 80, 50 };
+    cout << solution(people, 100);
+    return 0;
+}
