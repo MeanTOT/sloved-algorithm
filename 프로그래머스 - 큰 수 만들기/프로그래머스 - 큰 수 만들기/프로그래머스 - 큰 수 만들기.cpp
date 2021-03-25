40f751377efe4602882fbed47a08aaba37da63eb
Miracle Worker <dpalstn91@gmail.com>
2021-03-25 17:51:01 +0900

    Add new algorithm

---
+++ 프로그래머스 - 큰 수 만들기/프로그래머스 - 큰 수 만들기/프로그래머스 - 큰 수 만들기.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) 
+{
+    while (k-- > 0)
+    {        
+        for (int i = 0; i < number.size() - 1; i++)
+        {
+            if (number[i] < number[i + 1])
+            {
+                number.erase(i, 1);    
+                break;
+            }
+
+            if (i + 1 == number.size() - 1 && number[i + 1] < number[i])
+            {
+                number.erase(i + 1, 1);
+                break;
+            }
+        }        
+    }
+
+    return number;
+}
+
+int main()
+{
+    solution("1941", 2);
+
+    return 0;
+}
