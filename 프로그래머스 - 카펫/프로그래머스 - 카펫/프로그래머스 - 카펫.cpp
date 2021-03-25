a59de7045188fe3b25306b5afb1375ca44dd6afd
Miracle Worker <dpalstn91@gmail.com>
2021-03-25 13:59:48 +0900

    Add new algorithm

---
+++ 프로그래머스 - 카펫/프로그래머스 - 카펫/프로그래머스 - 카펫.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) 
+{
+    vector<int> answer;
+
+    int widthBlocks = brown - 2;
+    int heightBlocks = 2;
+
+    while (widthBlocks >= heightBlocks)
+    {
+        int width = widthBlocks / 2;
+        int height = (heightBlocks / 2) + 2;
+
+        if ((width * height) - brown == yellow)
+        {
+            answer.push_back(width);
+            answer.push_back(height);
+            break;
+        }
+
+        widthBlocks -= 2;
+        heightBlocks += 2;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(24, 24);
+
+    return 0;
+}
