7888b3e067219216c01d79ae6d2a634560eb875d
Miracle Worker <dpalstn91@gmail.com>
2021-04-02 19:04:05 +0900

    Add new algorithm

---
+++ 프로그래머스 - 도둑질/프로그래머스 - 도둑질/프로그래머스 - 도둑질.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> money) {
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    vector<int> money = { 90, 0, 0, 95, 1, 1 };
+
+    solution(money);
+}
