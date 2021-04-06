9c3a8dad891f86d386b1ea268b9f26aabb7ccce0
Miracle Worker <dpalstn91@gmail.com>
2021-04-06 15:20:48 +0900

    Add new algorithm

---
+++ 백준 - 설탕 배달/백준 - 설탕 배달/백준 - 설탕 배달.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>   
+
+using namespace std;
+
+int solution(int N)
+{
+    int answer = 0;
+
+    while (N % 5 != 0)
+    {
+        N -= 3;
+        answer++;
+        if (N <= 0)
+            break;
+    }
+    if (N < 0)
+        return -1;
+    if (N % 5 == 0)
+        answer += N / 5;
+
+    return answer;
+}
+
+int main()
+{
+    int N = 0;
+    cin >> N;
+    cout << solution(N);
+}
