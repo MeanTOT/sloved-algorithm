995f59b27d64b004aa0699dc5cf201f48ef20b8b
Miracle Worker <dpalstn91@gmail.com>
2021-04-06 17:02:50 +0900

    Add new algorithm

---
+++ 백준 - 1, 2, 3 더하기/백준 - 1, 2, 3 더하기/백준 - 1, 2, 3 더하기.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int Answer = 0;
+
+void solution(int current, int N)
+{
+	if (current > N)
+		return;
+	else if (current == N)
+	{
+		Answer++;
+		return;
+	}
+
+	solution(current + 1, N);
+	solution(current + 2, N);
+	solution(current + 3, N);
+}
+int main()
+{
+	int T = 0;
+	vector<int> Ns;
+	cin >> T;
+	Ns.resize(T);
+	for (int i = 0; i < Ns.size(); i++)
+	{
+		cin >> Ns[i];
+	}
+	for (int i = 0; i < Ns.size(); i++)
+	{
+		Answer = 0;
+		solution(0, Ns[i]);
+		cout << Answer << endl;
+	}
+}
