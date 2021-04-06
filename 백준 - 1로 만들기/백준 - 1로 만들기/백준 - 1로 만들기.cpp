73863560a33a2b482ebe1da2c9ad84fc8808f62e
Miracle Worker <dpalstn91@gmail.com>
2021-04-06 16:43:27 +0900

    Add new algorithm

---
+++ 백준 - 1로 만들기/백준 - 1로 만들기/백준 - 1로 만들기.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>   
+#include <memory.h>
+
+using namespace std;
+
+int cache[1000001];
+
+int solution(int N)
+{
+	memset(cache, 0, sizeof(cache));	
+	cache[1] = 0;
+	for (int i = 2; i <= N; i++)
+	{
+		cache[i] = cache[i - 1] + 1;
+		if (i % 2 == 0) cache[i] = min(cache[i], cache[i / 2] + 1);
+		if (i % 3 == 0) cache[i] = min(cache[i], cache[i / 3] + 1);
+	}
+	return cache[N];
+}
+
+int main()
+{
+	int N = 0;
+	cin >> N;
+	cout << solution(N);
+}
