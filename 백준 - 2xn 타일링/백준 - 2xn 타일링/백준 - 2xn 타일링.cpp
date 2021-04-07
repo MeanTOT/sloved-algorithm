c0c8155110907ffdf97864cf24c0d3a84101e09f
Miracle Worker <dpalstn91@gmail.com>
2021-04-07 11:57:40 +0900

    Add new algorithm

---
+++ 백준 - 2xn 타일링/백준 - 2xn 타일링/백준 - 2xn 타일링.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <memory.h>
+using namespace std;
+int cache[1001];
+int solution(int N)
+{
+	if (N < 0) return 0;
+	if (N == 0) return 1;
+
+	int& result = cache[N];
+	if (result != 0)
+		return result;
+
+	result += solution(N - 1);
+	result += solution(N - 2);
+
+	return result % 10007;
+}
+int main()
+{
+	int N = 0;
+	cin >> N;
+	memset(cache, 0, sizeof(cache));
+	cout << solution(N);
+}
