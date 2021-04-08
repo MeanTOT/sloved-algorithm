35560838630d2fa0d0cc3d81782ffa1f94fd789c
Miracle Worker <dpalstn91@gmail.com>
2021-04-08 13:13:22 +0900

    Add new algorithm

---
+++ 백준 - 피보나치 수 5/백준 - 피보나치 수 5/백준 - 피보나치 수 5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <memory.h>
+using namespace std;
+int cache[21];
+int fibo(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int& result = cache[n];
+    if (result != -1) return result;
+    return fibo(n - 1) + fibo(n - 2);
+}
+int main()
+{
+    int N = 0;
+    memset(cache, -1, sizeof(cache));
+    cin >> N;
+    cout << fibo(N);
+}
