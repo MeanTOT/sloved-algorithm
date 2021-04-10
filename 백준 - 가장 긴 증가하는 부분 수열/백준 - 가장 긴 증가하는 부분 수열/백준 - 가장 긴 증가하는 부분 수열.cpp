5f181636bfc2b8777535bb8fec34d58bf3b387df
Miracle Worker <dpalstn91@gmail.com>
2021-04-10 13:47:57 +0900

    Add new algorithm

--- 백준 - 가장 긴 증가하는 부분 수열/백준 - 가장 긴 증가하는 부분 수열/백준 - 가장 긴 증가하는 부분 수열.cpp
+++ 백준 - 가장 긴 증가하는 부분 수열/백준 - 가장 긴 증가하는 부분 수열/백준 - 가장 긴 증가하는 부분 수열.cpp
@@ -1,58 +1,42 @@
 ﻿#include <iostream>
 #include <vector>
+#include <memory.h>
 using namespace std;
 int A = 0;
 int MaxLength = 0;
+int cache[1001];
 vector<int> numbers;
-//void dp(int previous, int index, int length)
-//{
-//	if (index == numbers.size())
-//	{
-//		MaxLength = max(MaxLength, length);
-//		return;
-//	}
-//	
-//	if (numbers[index] > previous)
-//	{
-//		dp(numbers[index], index + 1, length + 1);
-//	}
-//	else
-//	{
-//		dp(numbers[index], index + 1, 1);
-//	}
-//
-//	dp(previous, index + 1, length);
-//}
-void Search(int previous, int index, int length)
+int dp(int previous, int index, int length)
 {
 	if (index == numbers.size())
-	{
-		MaxLength = max(MaxLength, length);
-		return;
-	}
-
+		return length;
+	
+	length++;
+	int& result = cache[index];
 	if (numbers[index] > previous)
-	{
-		Search(numbers[index], index + 1, length + 1);
+	{		
+		if (result != 0) return length + result;
+		result = max(result,dp(numbers[index], index + 1, length));		
 	}
-	else
-	{
-		Search(previous, index + 1, length);
-	}
+	
+	result = max(result, dp(previous, index + 1, length - 1));
+
+	return result;
 }
 int solution()
 {
-	//dp(0, 0, 0);
-	for (int i = 0; i < numbers.size(); i++)
+	int result = 1;
+	for (int i = numbers.size() - 1; i >= 0; i--)
 	{
-		Search(0, i, 1);
+		result = max(result, dp(0, i, 0));
 	}
-	return MaxLength;
+	return result;
 }
 int main()
 {
 	cin >> A;
 	numbers.resize(A);
+	memset(cache, 0, sizeof(cache));
 	for (int i = 0; i < A; i++)
 	{
 		cin >> numbers[i];
---
+++ 백준 - 피보나치 수 2/백준 - 피보나치 수 2/백준 - 피보나치 수 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <memory.h>
+using namespace std;
+long long cache[91];
+long long dp(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    
+    long long& result = cache[n];
+    if (result != -1) return result;
+
+    return result = dp(n - 1) + dp(n - 2);
+}
+int solution(int n)
+{
+    return dp(n);
+}
+int main()
+{
+    int N = 0;
+    memset(cache, -1, sizeof(cache));
+    cin >> N;
+    printf("%lld", dp(N));
+}
