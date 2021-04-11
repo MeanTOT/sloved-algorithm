2ceb478d2d61a02a151a7d9a68185a4387425ac0
Miracle Worker <dpalstn91@gmail.com>
2021-04-11 13:09:20 +0900

    Add new algorithm

---
+++ 백준 - 쉬운 계단 수/백준 - 쉬운 계단 수/백준 - 쉬운 계단 수.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <memory.h>
+using namespace std;
+int N = 0;
+int const MOD = 1000000000;
+long long cache[10][101];
+long long dp(int number, int remain)
+{
+    if (remain == 0)
+        return 1;
+
+    long long& result = cache[number][remain];
+    if (result != 0) return result;
+
+    if (number == 0)
+        result += dp(number + 1, remain - 1);
+    else if (number == 9)
+        result += dp(number - 1, remain - 1);
+    else
+    {
+        result += dp(number + 1, remain - 1);
+        result += dp(number - 1, remain - 1);
+    }
+
+    return result % MOD;
+}
+int solution()
+{
+    long long result = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        result += dp(i, N - 1);
+    }
+
+    return result % MOD;
+}
+int main()
+{
+    cin >> N;
+    memset(cache, 0, sizeof(cache));
+    printf("%lld", solution());
+}
---
+++ 백준 - 연속0합/백준 - 연속0합/백준 - 연속0합.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory.h>
+using namespace std;
+int N = 0;
+vector<int> numbers;
+int MaxNumber = -1001;
+int cache[100000];
+//int dp(int start, int end)
+//{
+//    if (start == end)
+//        return numbers[start];
+//
+//    int half = (start + end) / 2;
+//    int result1 = dp(start, half);
+//    int result2 = dp(half + 1, end);
+//    int result3 = result1 + result2;
+//    MaxNumber = max(max(result1, result2), result3);
+//    return result3;
+//}
+int dp(int previsou, int index)
+{
+    if (index == numbers.size()) return 0;    
+    MaxNumber = max(MaxNumber, previsou + numbers[index]);
+    dp(previsou + numbers[index], index + 1);
+}
+int solution()
+{
+    /*for (int i = 0; i < N; i++)
+    {
+        MaxNumber = dp(0, i);
+    }*/
+    cache[0] = numbers[0] + numbers[1];
+    MaxNumber = cache[0];
+    for (int i = 1; i < N - 1; i++)
+    {
+        cache[i] = cache[i - 1] + numbers[i + 1];
+        MaxNumber = max(MaxNumber, cache[i]);
+    }
+    int sub = 0;
+    for (int i = 0; i < N - 2; i++)
+    {
+        sub += numbers[i];
+        for (int j = i + 1; j < N - 1; j++)
+        {
+            MaxNumber = max(MaxNumber, cache[j] - sub);
+        }
+        MaxNumber = max(MaxNumber, numbers[i]);
+    }
+    MaxNumber = max(MaxNumber, numbers[N - 2]);
+    MaxNumber = max(MaxNumber, numbers[N - 1]);
+    return MaxNumber;
+}
+int main()
+{
+    cin >> N;
+    numbers.resize(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> numbers[i];
+    }
+    cout << solution();
+}
