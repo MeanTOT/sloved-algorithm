775b3adec0c39e25f825a35d28b919a7500f8ccd
Miracle Worker <dpalstn91@gmail.com>
2021-04-12 16:19:39 +0900

    Add new algorithm

---
+++ 백준 - ATM/백준 - ATM/백준 - ATM.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N = 0;
+vector<int> P;
+int solution()
+{
+	int result = 0;
+	int sum = 0;
+	sort(P.begin(), P.end());
+	for (int i = 0; i < N; i++)
+	{
+		sum += P[i];
+		result += sum;
+	}
+	return result;
+}
+int main()
+{
+	cin >> N;
+	P.resize(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> P[i];
+	}
+	cout << solution();
+}
