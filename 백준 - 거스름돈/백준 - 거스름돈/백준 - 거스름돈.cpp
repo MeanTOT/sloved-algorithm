cb457fd622d09ed6ff38d97f169a0a583149c392
Miracle Worker <dpalstn91@gmail.com>
2021-04-13 15:32:27 +0900

    Add new algorithm

---
+++ 백준 - 거스름돈/백준 - 거스름돈/백준 - 거스름돈.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int payedMoney = 0;
+int solution()
+{
+    int result = 0;
+    int change = 1000 - payedMoney;
+    int changes[6] = { 500, 100, 50, 10, 5, 1 };
+    int index = 0;
+    while (change > 0)
+    {
+        if (change - changes[index] < 0)
+            index++;
+        else
+        {
+            change -= changes[index];
+            result++;
+        }
+    }
+    return result;
+}
+int main()
+{
+    cin >> payedMoney;
+    cout << solution();
+}
