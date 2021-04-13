8c615156839e996ba8985709db0314f4f14328d8
Miracle Worker <dpalstn91@gmail.com>
2021-04-13 18:17:14 +0900

    Add new algirhtm

---
+++ 백준 - 로프/백준 - 로프/백준 - 로프.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N = 0;
+vector<int> weights;
+int Solution()
+{
+    sort(weights.begin(), weights.end(), greater<int>());
+    int result = weights[0];    
+    for (int i = 1; i < weights.size(); i++)
+    {
+        int lopeCount = (i + 1);
+        if (result <= weights[i] * lopeCount)
+            result = weights[i] * lopeCount;
+    }
+    return result;
+}
+int main()
+{
+    cin >> N;
+    weights.resize(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> weights[i];
+    }
+    cout << Solution();
+}
---
+++ 백준 - 신입 사원/백준 - 신입 사원/백준 - 신입 사원.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int T = 0;
+int N = 0;
+vector<pair<int, int>> specs;
+vector<int> answers;
+int solution()
+{
+	sort(specs.begin(), specs.end());
+	int result = 0;
+	int compSpec = 100001;
+	for (int i = 0; i < N; i++)
+	{
+		if (compSpec > specs[i].second)
+		{
+			compSpec = specs[i].second;
+			result++;
+		}
+	}
+	return result;
+}
+int main()
+{
+	cin >> T;
+	while (T--)
+	{
+		cin >> N;
+		for (int i = 0; i < N; i++)
+		{
+			int spec1 = 0;
+			int spec2 = 0;
+			cin >> spec1 >> spec2;
+			specs.push_back(make_pair(spec1, spec2));
+		}
+		answers.push_back(solution());
+		specs.clear();
+	}
+
+	for (int i = 0; i < answers.size(); i++)
+	{
+		cout << answers[i] << endl;
+	}
+}
