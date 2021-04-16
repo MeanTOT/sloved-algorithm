2ef14f485190aae75b6d5f23f1877b0e2b5fbbdd
Miracle Worker <dpalstn91@gmail.com>
2021-04-16 15:17:20 +0900

    Add new algirhtm

---
+++ 백준 - 캠핑/백준 - 캠핑/백준 - 캠핑.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int L, P, V = 0;
+vector<int> answers;
+int solution()
+{
+    int result = 0;
+    while (V > 0)
+    {
+        if (V - L > 0)
+        {
+            result += L;
+            V -= P;
+        }
+        else
+        {
+            if (V > 0)
+                result += V;
+            break;
+        }
+    }
+    return result;
+}
+int main()
+{
+    int caseCount = 0;
+    while (true)
+    {
+        cin >> L >> P >> V;
+        if (L == 0 && P == 0 && V == 0)
+            break;
+        else
+            answers.push_back(solution());            
+    }    
+    for (int i = 0; i < answers.size(); i++)
+    {
+        cout << "Case " << ++caseCount << ": " << answers[i] << endl;
+    }
+}
