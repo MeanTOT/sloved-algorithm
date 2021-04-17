9e66c320d9dd17afe8a0949c64300a817453cf3d
Miracle Worker <dpalstn91@gmail.com>
2021-04-18 00:05:49 +0900

    Add new algorithm

---
+++ 백준 - 막대기/백준 - 막대기/백준 - 막대기.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int X = 0;
+    int result = 0;
+    cin >> X;
+    for (int i = 0; i < 7; i++)
+    {
+        if (X % 2 == 1) result++;
+        X >>= 1;
+    }
+    cout << result;
+}
