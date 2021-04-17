c8bc14548fa2f38459b20b672cdaddbb8c1c891b
Miracle Worker <dpalstn91@gmail.com>
2021-04-18 03:41:56 +0900

    Add new algorithm

---
+++ 백준 - 돌 게임2/백준 - 돌 게임2/백준 - 돌 게임2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string player = "SK";
+    int N = 0;
+    cin >> N;
+    while (true)
+    {
+        if (N - 3 >= 3) N -= 3;
+        else if (N - 3 < 3) N -= 1;
+
+        if (N == 0)
+        {
+            if (player == "SK") player = "CY";
+            else if (player == "CY") player = "SK";
+            break;
+        }
+        if (player == "SK") player = "CY";
+        else if (player == "CY") player = "SK";
+    }
+    cout << player;
+}
