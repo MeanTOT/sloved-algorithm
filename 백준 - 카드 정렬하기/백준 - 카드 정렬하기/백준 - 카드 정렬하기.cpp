296e0ab3b6fe6952d7a1f3a5f12608f595a8a006
Miracle Worker <dpalstn91@gmail.com>
2021-04-17 14:54:04 +0900

    Add new algorithm

---
+++ 백준 - 카드 정렬하기/백준 - 카드 정렬하기/백준 - 카드 정렬하기.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int N = 0;
+priority_queue<int, vector<int>, greater<int>> bunchOfCards;
+int solution()
+{
+    if (bunchOfCards.size() == 1) return 0;
+
+    int result = 0;
+    while (bunchOfCards.size() != 1)
+    {
+        int cards_1 = bunchOfCards.top(); bunchOfCards.pop();
+        int cards_2 = bunchOfCards.top(); bunchOfCards.pop();
+        result += cards_1 + cards_2;
+        bunchOfCards.push(cards_1 + cards_2);
+    }
+
+    return result;
+}
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int amount = 0;
+        cin >> amount;
+        bunchOfCards.push(amount);
+    }
+    printf("%lld", solution());
+}
---
+++ 프로그래머스 - 모두 0으로 만들기/프로그래머스 - 모두 0으로 만들기/프로그래머스 - 모두 0으로 만들기.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+using namespace std;
+long long solution(vector<int> a, vector<vector<int>> edges)
+{
+    long long answer = 0;
+
+    int currPattern = 0;
+    int sum = 0;
+    for (int i = 0; i < edges.size(); i++)
+    {
+        answer += abs(a[edges[i][currPattern]]);
+        sum += a[edges[i][currPattern]];
+
+        if (currPattern == 0) currPattern = 1;
+        else if (currPattern == 1) currPattern = 0;
+    }
+    if (sum != 0) return -1;
+    return answer;
+}
+int main()
+{
+    vector<int> a = {-5, 0, 2, 1, 2};
+    vector<vector<int>> edges = { {0, 1},{3, 4},{2, 3},{0, 3} };
+
+    std::cout << solution(a, edges);
+}
