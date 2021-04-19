688de59347acbcb2d30c70a655b686f281fe19d6
Miracle Worker <dpalstn91@gmail.com>
2021-04-19 12:45:15 +0900

    Add new algorithm

--- 프로그래머스 - 모두 0으로 만들기/프로그래머스 - 모두 0으로 만들기/프로그래머스 - 모두 0으로 만들기.cpp
+++ 프로그래머스 - 모두 0으로 만들기/프로그래머스 - 모두 0으로 만들기/프로그래머스 - 모두 0으로 만들기.cpp
@@ -1,29 +1,44 @@
 ﻿#include <string>
 #include <vector>
 #include <iostream>
-#include <map>
 using namespace std;
-long long solution(vector<int> a, vector<vector<int>> edges)
+vector<vector<int>> tree;
+vector<long long> A;
+long long answer = 0;
+void DFS(vector<vector<int>>& edges, int curr, int parent)
 {
-    long long answer = 0;
-
-    int currPattern = 0;
-    int sum = 0;
-    for (int i = 0; i < edges.size(); i++)
+    for (size_t i = 0; i < tree[curr].size(); i++)
     {
-        answer += abs(a[edges[i][currPattern]]);
-        sum += a[edges[i][currPattern]];
-
-        if (currPattern == 0) currPattern = 1;
-        else if (currPattern == 1) currPattern = 0;
+        if (tree[curr][i] != parent)
+        {
+            DFS(edges, tree[curr][i], curr);
+            A[curr] += A[tree[curr][i]];
+            answer += abs(A[tree[curr][i]]);
+            A[tree[curr][i]] = 0;
+        }        
     }
-    if (sum != 0) return -1;
+}
+long long solution(vector<int> a, vector<vector<int>> edges)
+{    
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        A.push_back((long long)a[i]);
+        answer += a[i];
+    }
+    if (answer != 0) return -1;    
+    tree.resize(a.size());
+    for (size_t i = 0; i < edges.size(); ++i)
+    {
+        tree[edges[i][0]].push_back(edges[i][1]);
+        tree[edges[i][1]].push_back(edges[i][0]);
+    }
+    DFS(edges, 0, 0);
     return answer;
 }
 int main()
 {
-    vector<int> a = {-5, 0, 2, 1, 2};
-    vector<vector<int>> edges = { {0, 1},{3, 4},{2, 3},{0, 3} };
+    vector<int> a = {0, 0, 0, 0, 0};
+    vector<vector<int>> edges = { {0, 1}, {3, 4}, {2, 3}, {0, 3} };
 
     std::cout << solution(a, edges);
 }
