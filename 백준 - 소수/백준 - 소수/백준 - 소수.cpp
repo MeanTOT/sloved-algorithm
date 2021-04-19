6f447544a410a21589624429590507048df4c75e
Miracle Worker <dpalstn91@gmail.com>
2021-04-19 18:38:40 +0900

    Add new algirithm

---
+++ 백준 - 소수/백준 - 소수/백준 - 소수.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int M, N = 0;
+int main()
+{
+    cin >> M;
+    cin >> N;
+    int primeSum = 0;
+    int miniPrime = 10001;
+    for (int i = M; i <= N; ++i)
+    {
+        if (i == 1) continue;
+        if (i == 2)
+        {
+            miniPrime = 2;
+            primeSum = 2;
+            continue;
+        }
+        int sqrtI = (int)sqrt(i);
+        bool isPrime = true;
+        for (int j = 2; j <= sqrtI; ++j)
+        {
+            if (i % j == 0) 
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+        {
+            miniPrime = min(miniPrime, i);
+            primeSum += i;
+        }
+    }
+    if (primeSum == 0) printf("-1");
+    else printf("%d\n%d", primeSum, miniPrime);
+}
---
+++ 백준 - 최대공배수/백준 - 최대공배수/백준 - 최대공배수.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int GDC(int A, int B)
+{
+    if (B == 0) return A;
+    return GDC(B, A % B);
+}
+int main()
+{
+    int T = 0;
+    int A, B = 0;
+    cin >> T;
+    while (T--)
+    {
+        cin >> A >> B;
+        printf("%d\n", (A * B) / GDC(A, B));
+    }
+}
