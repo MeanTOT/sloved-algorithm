440f8d6fc3593548f86e94866940962b8aa2ec21
Miracle Worker <dpalstn91@gmail.com>
2021-04-20 17:46:25 +0900

    Add new algorithm

---
+++ 백준 - 날짜 계산/백준 - 날짜 계산/백준 - 날짜 계산.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int GCD(int A, int B)
+{
+	if (B == 0) return A;
+	return GCD(B, A % B);
+}
+int main()
+{
+	int N = 0;
+	cin >> N;
+	vector<int> aliquots(N);	
+	for (int i = 0; i < N; i++) cin >> aliquots[i];	
+	sort(aliquots.begin(), aliquots.end());
+
+	int num = aliquots[aliquots.size() -1];
+	cout << aliquots[aliquots.size() - 1] * aliquots[0];
+}
---
+++ 백준 - 소인수분해/백준 - 소인수분해/백준 - 소인수분해.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+int N = 0;
+vector<int> result;
+void solution()
+{
+	int sqrtN = (int)sqrt(N);
+	for (int i = 2; i <= sqrtN; ++i)
+	{
+		while (N % i == 0)
+		{
+			N /= i;
+			result.push_back(i);
+		}		
+	}
+	if (N > 1) result.push_back(N);
+}
+int main()
+{
+	cin >> N;
+	solution();
+	for (size_t i = 0; i < result.size(); i++) printf("%d\n", result[i]);
+}
