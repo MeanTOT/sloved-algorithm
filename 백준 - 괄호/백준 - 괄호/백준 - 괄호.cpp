8ff1ca110248921e4a5881443cbb242c8dc76901
Miracle Worker <dpalstn91@gmail.com>
2021-04-22 13:12:38 +0900

    Add new algorithm

---
+++ 백준 - 괄호/백준 - 괄호/백준 - 괄호.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+string solution(string& vps)
+{
+    stack<char> token;
+    for (int i = 0; i < vps.length(); ++i)
+    {
+        if (vps[i] == '(') token.push(vps[i]);
+        else if (vps[i] == ')') if (token.empty()) return "NO";
+            else token.pop();
+    }
+    if (!token.empty()) return "NO";
+    return "YES";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    int T = 0;
+    cin >> T;
+    while (T--)
+    {
+        string vps = "";
+        cin >> vps;
+        cout << solution(vps) << endl;
+    }
+}
---
+++ 백준 - 요세푸스 문제/백준 - 요세푸스 문제/백준 - 요세푸스 문제.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int N, K = 0;
+vector<int> peaple;
+vector<int> answer;
+int main()
+{
+	int a = scanf("%d %d", &N, &K);
+	for (int i = 1; i <= N; i++)
+		peaple.push_back(i);
+	
+	int curr = -1;
+	int count = 0;
+	int remain = N;
+	while (true)
+	{		
+		curr++;
+		if (curr == N) curr = 0;
+		if (peaple[curr] != -1)
+			count++;
+		if (count == K)
+		{
+			answer.push_back(peaple[curr]);
+			peaple[curr] = -1;
+			count = 0;
+			remain--;
+
+			if (remain == 0)break;
+		}
+	}
+
+	printf("<");
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d", answer[i]);
+		if (i != N - 1) printf(", ");
+	}
+	printf(">");
+}
