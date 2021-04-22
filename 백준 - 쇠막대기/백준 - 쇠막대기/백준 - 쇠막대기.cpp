884c9db435cc66b4bba8b3e3b926ef17759994fa
Miracle Worker <dpalstn91@gmail.com>
2021-04-22 20:16:41 +0900

    Add new algorithm

---
+++ 백준 - 쇠막대기/백준 - 쇠막대기/백준 - 쇠막대기.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+string input;
+int answer = 0;
+int Stack(int& index)
+{
+    if (index == -1)
+    {
+        Stack(++index);
+        return 0;
+    }
+    int laser = 0;
+    while (input[index] == '(')
+    {
+        laser += Stack(++index);
+    }
+    if (input[index - 1] == '(' && input[index] == ')')
+    {
+        index++;
+        return 1;
+    }
+    if (input[index - 1] == ')' && input[index] == ')')
+    {
+        index++;
+        answer += laser + 1;
+    }
+    return laser;
+}
+int main()
+{
+    cin >> input;
+    int index = -1;
+    Stack(index);
+    cout << answer;
+}
---
+++ 백준 - 제로/백준 - 제로/백준 - 제로.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    int K = 0;
+	int answer = 0;
+	stack<int> numbers;
+    cin >> K;
+	while (K--)
+	{
+		int number = 0;
+		cin >> number;
+		if (number == 0)
+		{
+			answer -= numbers.top();
+			numbers.pop();
+		}
+		else
+		{
+			answer += number;
+			numbers.push(number);
+		}
+	}
+	cout << answer;
+}
