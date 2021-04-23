0d494dbb85c4268195770bd86f96f9cca7098ad3
Miracle Worker <dpalstn91@gmail.com>
2021-04-23 19:09:38 +0900

    Add new algorithm

---
+++ 백준 - 에디터/백준 - 에디터/백준 - 에디터.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    string str = "";
+    int M = 0;
+    cin >> str;
+    cin >> M;
+    while (M--)
+    {
+        char c = 0;
+        char p = 0;
+        cin >> c;
+        if (c == 'P') 
+            cin >> p;
+    }
+    string result = "X";
+    cout << result;
+}
