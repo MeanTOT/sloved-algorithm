0513281fe80a93ca0272b11f47b6eed6b6ea55e5
Miracle Worker <dpalstn91@gmail.com>
2021-04-26 14:50:01 +0900

    Add new algorithm

---
+++ 백준 - 접미사 배열/백준 - 접미사 배열/백준 - 접미사 배열.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+string s = "";
+bool Comp(int i, int j)
+{	
+	return strcmp(s.c_str() + i, s.c_str() + j) < 0;
+}
+int main()
+{
+	vector<int> perm;
+	cin >> s;
+	for (size_t i = 0; i < s.length(); ++i) perm.push_back(i);
+	sort(perm.begin(), perm.end(), Comp);	
+}
