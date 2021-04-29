e13a3cc897c0f38565f4fb31f10f1cc1f0ceeb0a
Miracle Worker <dpalstn91@gmail.com>
2021-04-29 19:43:25 +0900

    Add new algorithm

---
+++ 백준 - 구간 합 구하기 4/백준 - 구간 합 구하기 4/백준 - 구간 합 구하기 4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+    int N, M = 0;
+	vector<int> arr;
+    cin >> N >> M;
+	int value = 0;
+	while (N--)
+	{
+		int x = 0;
+		cin >> x;
+		value += x;
+		arr.push_back(value);
+	}
+	while (M--)
+	{
+		int i, j, result = 0;
+		cin >> i >> j;
+		if (i == 1) printf("%d\n", arr[j - 1]);
+		else printf("%d\n", arr[j - 1] - arr[i - 2]);
+	}
+}
---
+++ 백준 - 절댓값 힙/백준 - 절댓값 힙/백준 - 절댓값 힙.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    int N = 0;
+    cin >> N;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+    while (N--)
+    {
+        int x = 0;
+        cin >> x;
+        if (x != 0)
+            heap.push(make_pair(abs(x), x));
+        else
+        {
+            if (heap.size() == 0)
+            {
+                printf("0\n");
+                continue;
+            }
+            printf("%d\n", heap.top().second);
+            heap.pop();
+        }
+    }
+}
