5a7b2754a45d9d4c78ef117e64ab614a4379ca00
Miracle Worker <dpalstn91@gmail.com>
2021-05-01 22:48:15 +0900

    Add new algorithm{

---
+++ 백준 - 최소, 최대/백준 - 최소, 최대/백준 - 최소, 최대.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    int N = 0;
+    int Max = -1000001;
+    int Min = 1000001;
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        int number = 0;
+        cin >> number;
+        if (number > Max) Max = number;
+        if (number < Min) Min = number;        
+    }
+    cout << Min << " " << Max << endl;
+}
