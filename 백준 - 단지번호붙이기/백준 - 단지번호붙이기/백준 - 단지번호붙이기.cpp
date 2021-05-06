c9b840f664a205c74751dfffd03311dce75ba50d
Miracle Worker <dpalstn91@gmail.com>
2021-05-06 14:07:03 +0900

    Add new algorithm

---
+++ 백준 - 단지번호붙이기/백준 - 단지번호붙이기/백준 - 단지번호붙이기.cpp
@@ -0,0 +1,90 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int mapSize = 0;
+vector<vector<int>> map;
+vector<vector<int>> adj;
+vector<bool> visited;
+vector<int> answers;
+int DFS(int here)
+{
+    visited[here] = true;
+    int result = 1;
+    for (int i = 0; i < adj[here].size(); ++i)
+    {
+        int there = adj[here][i];
+        if (!visited[there])
+            result += DFS(there);
+    }    
+
+    return result;
+}
+
+int DFSAll()
+{
+    int result = 0;
+    visited = vector<bool>(adj.size(), false);
+    for (int i = 0; i < adj.size(); ++i)
+    {
+        if (!visited[i])
+        {            
+            int answer = DFS(i);
+
+            if (answer > 1)
+            {
+                result++;
+                answers.push_back(answer);
+            }
+            if (answer == 1 && map[i / mapSize][i % mapSize])
+            {
+                result++;
+                answers.push_back(1);
+            }
+        }
+    }
+    return result;
+}
+int main()
+{
+    cin >> mapSize;
+    map.resize(mapSize);
+    adj.resize(mapSize * mapSize);
+    visited.resize(mapSize * mapSize);
+    for (int i = 0; i < mapSize; ++i)
+    {
+        for (int j = 0; j < mapSize; ++j)
+        {
+            int isHome = 0;
+            int a = scanf("%1d", &isHome);
+            map[i].push_back(isHome);
+        }
+    }
+
+    for (int i = 0; i < mapSize; ++i)
+    {
+        for (int j = 0; j < mapSize; ++j)
+        {
+            if (map[i][j])
+            {
+                if (i - 1 >= 0 && map[i - 1][j]) // up
+                    adj[i* mapSize + j].push_back((i - 1) * mapSize + j);
+                if (i + 1 < mapSize && map[i + 1][j]) // down
+                    adj[i * mapSize + j].push_back((i + 1) * mapSize + j);
+                if (j - 1 >= 0 && map[i][j - 1]) // left
+                    adj[i * mapSize + j].push_back(i * mapSize + j - 1);
+                if (j + 1 < mapSize && map[i][j + 1]) // right
+                    adj[i * mapSize + j].push_back(i * mapSize + j + 1);
+            }
+        }
+    }
+
+    cout << DFSAll() << endl;
+    sort(answers.begin(), answers.end());
+    for (int i = 0; i < answers.size(); ++i)
+    {
+        cout << answers[i] << endl;
+    }
+}
---
+++ 백준 - 바이러스/백준 - 바이러스/백준 - 바이러스.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory.h>
+using namespace std;
+vector<int> adj[102];
+bool visited[102];
+int computers = 0, edge = 0, answer = 0;
+void DFS(int here)
+{
+	answer++;
+	visited[here] = true;
+	for (int i = 0; i < adj[here].size(); ++i)
+	{
+		int there = adj[here][i];
+		if (!visited[there])
+			DFS(there);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin >> computers;
+	cin >> edge;
+	for (int i = 0; i < edge; ++i)
+	{
+		int a = 0, b = 0;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	DFS(1);
+	cout << answer - 1;
+}
