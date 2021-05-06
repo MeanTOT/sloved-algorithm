9610038eb0ee5d113a1735943c0885f39877dabb
Miracle Worker <dpalstn91@gmail.com>
2021-05-06 18:42:13 +0900

    Add new algorithm

---
+++ 백준 - 셀프 넘버/백준 - 셀프 넘버/백준 - 셀프 넘버.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+bool NotSelfNumber[10001];
+void d(int n)
+{
+	int sum = 0;
+	int result = n;
+	while (n / 10 != 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	result += sum + n;	
+	if (result <= 10000)
+	{
+		NotSelfNumber[result] = true;
+		d(result);
+	}
+	return;
+}
+
+int main()
+{
+	for (int i = 1; i <= 10000; ++i)
+	{
+		if (!NotSelfNumber[i])
+		{
+			d(i);
+			printf("%d\n", i);
+		}
+	}	
+}
---
+++ 백준 - 한수/백준 - 한수/백준 - 한수.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+bool ap(int n)
+{
+    if (n < 100) return true;
+
+    vector<int> aps;
+    while (n >= 10)
+    {
+        aps.push_back(n % 10);
+        n /= 10;
+    }
+    aps.push_back(n);
+    bool result = true;
+    int sub = aps[0] - aps[1];
+    for (int i = 1; i < aps.size() - 1; i++)
+    {
+        if (aps[i] - aps[i + 1] != sub)
+        {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int N = 0, result = 0;
+    cin >> N;
+    for (int i = 1; i <= N; ++i)
+    {
+        if (ap(i)) result++;
+    }
+    cout << result;
+}
