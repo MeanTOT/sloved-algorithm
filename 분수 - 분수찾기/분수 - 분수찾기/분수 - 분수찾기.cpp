436c0cd87f2ae405b207dcdfb67d2bc3bcf477c7
Miracle Worker <dpalstn91@gmail.com>
2021-05-07 17:59:55 +0900

    Add new algorithm

---
+++ 분수 - 분수찾기/분수 - 분수찾기/분수 - 분수찾기.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int X = 0, n = 1, sum = 0;;
+    cin >> X;   
+    while (true)
+    {
+        sum = ((n + 1) * n) / 2;
+        if (sum >= X) break;
+        ++n;
+    }
+    if (n % 2 == 0) cout << n - (sum - X) << "/" << 1 + (sum - X);
+    else cout << 1 + sum - X << "/" << n - (sum - X);
+}
