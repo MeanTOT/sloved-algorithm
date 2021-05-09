7bf70818b08664001ea4ee51c2d66577df73ada9
Miracle Worker <dpalstn91@gmail.com>
2021-05-09 14:58:10 +0900

    Add new algorithm

---
+++ 백준 - 10250/백준 - 10250/백준 - 10250.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int T = 0, H = 0, W = 0, N = 0;
+    cin >> T;
+    while (T--)
+    {
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
+        cin >> H >> W >> N;        
+        if ((N % H) == 0) cout << (H * 100) + (N / H) << "\n";
+        else cout << ((N % H) * 100) + ((N / H) + 1) << "\n";        
+    }
+}
---
+++ 백준 - 2869/백준 - 2869/백준 - 2869.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int A = 0, B = 0, V = 0;
+	cin >> A >> B >> V;
+	if ((V - B) % (A - B) == 0) cout << (V - B) / (A - B);
+	else cout << (V - B) / (A - B) + 1;
+}
