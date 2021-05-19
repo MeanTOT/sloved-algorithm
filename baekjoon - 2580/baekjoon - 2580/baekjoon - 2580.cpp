12008843433fc2aafc2527b26191c526496ef786
Miracle Worker <dpalstn91@gmail.com>
2021-05-19 18:53:01 +0900

    Add new algorithm

--- baekjoon - 2580/baekjoon - 2580/baekjoon - 2580.cpp
+++ baekjoon - 2580/baekjoon - 2580/baekjoon - 2580.cpp
@@ -15,8 +15,11 @@ vector<bool> CreateNumbersVector(int target_i, int target_j)
             result[GameTable[i][target_j]] = true;
     }
 
-    while (target_i / 3 != 0) target_i--;
-    while (target_j / 3 != 0) target_j--;
+
+    if (target_i % 3 == 0) target_i -= 2;
+    if (target_j % 3 == 0) target_j -= 2;
+    while (target_i % 3 != 0) target_i--;
+    while (target_j % 3 != 0) target_j--;
     target_i++;
     target_j++;
 
@@ -31,17 +34,69 @@ vector<bool> CreateNumbersVector(int target_i, int target_j)
         if (target_j % 3 == 1)
         {
             target_i++;
+            target_j -= 3;
         }
     }
 
     return result;
 }
 
-void Solution(int target_i, int target_j)
+void Solution(int target_i, int target_j, bool& SudokuIsComplete)
 {
-    vector<bool> Numbers = CreateNumbersVector(target_i, target_j);
+    if (target_i == 10 && target_j == 1)
+    {
+        for (int i = 1; i <= 9; ++i)
+        {
+            int sum = 0;
+            for (int j = 1; j <= 9; ++j)
+            {
+                sum += GameTable[i][j];
+            }
+            if (sum != 45)
+            {
+                return;
+            }
+        }
 
-    int a = 0;
+        SudokuIsComplete = true;
+        return;
+    }
+
+    if (GameTable[target_i][target_j] == 0)
+    {
+        vector<bool> Numbers = CreateNumbersVector(target_i, target_j);
+        for (int i = 1; i < Numbers.size(); ++i)
+        {
+            if (Numbers[i] == false && SudokuIsComplete == false)
+            {
+                GameTable[target_i][target_j] = i;
+                if (target_j + 1 == 10)
+                {
+                    Solution(target_i + 1, 1, SudokuIsComplete);
+                }
+                else
+                {
+                    Solution(target_i, target_j + 1, SudokuIsComplete);
+                }
+
+                if (SudokuIsComplete == false)
+                {
+                    GameTable[target_i][target_j] = 0;
+                }
+            }
+        }
+    }
+    else
+    {
+        if (target_j + 1 == 10)
+        {
+            Solution(target_i + 1, 1, SudokuIsComplete);
+        }
+        else
+        {
+            Solution(target_i, target_j + 1, SudokuIsComplete);
+        }
+    }    
 }
 
 int main()
@@ -56,16 +111,25 @@ int main()
         }
     }
 
-
     for (int i = 1; i <= 9; ++i)
     {
         for (int j = 1; j <= 9; ++j)
         {
             if (GameTable[i][j] == 0)
             {
-                Solution(i, j);
+                bool SudokuIsComplete = false;
+                Solution(i, j, SudokuIsComplete);
                 break;
             }
         }
+    }    
+    
+    for (int i = 1; i <= 9; ++i)
+    {
+        for (int j = 1; j <= 9; ++j)
+        {
+            cout << GameTable[i][j] << " ";
+        }
+        cout << "\n";
     }
 }
