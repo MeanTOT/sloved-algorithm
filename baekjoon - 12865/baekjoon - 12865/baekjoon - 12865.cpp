8b1b3cc54d7916688177bb9d9bae43257526171c
Miracle Worker <dpalstn91@gmail.com>
2021-05-24 13:37:29 +0900

    Add new algorithm

--- baekjoon - 12865/baekjoon - 12865/baekjoon - 12865.cpp
+++ baekjoon - 12865/baekjoon - 12865/baekjoon - 12865.cpp
@@ -3,39 +3,41 @@
 #include <memory.h>
 using namespace std;
 int N = 0, K = 0, W = 0, V = 0;
-vector<pair<int, int>> things;
-int cache[101];
-int dp(int index, int weight)
+vector<pair<int, int>> Objects;
+int cache[101][100001];
+int DP(int start, int weight)
 {
-	int& ret = cache[index];
-	//if (ret != -1) return ret;
-	ret = things[index].second;
+    int& ret = cache[start][weight];
+    if (ret != -1) return ret;
+    ret = Objects[start].second;
 
-	for (int i = index + 1; i < N; ++i)
-	{
-		if (weight + things[i].first <= K)
-		{
-			ret = max(ret, things[index].second + dp(i, weight + things[i].first));
-		}
-	}
-	return ret;
+    for (size_t i = start + 1; i < Objects.size(); ++i)
+    {
+        if (weight + Objects[i].first <= K)
+        {
+            ret = max(ret, DP(i, weight + Objects[i].first) + Objects[start].second);
+        }
+    }
+
+    return ret;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	cin >> N >> K;
-	memset(cache, -1, sizeof(cache));
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> W >> V;
-		things.push_back(make_pair(W, V));
-	}
-	
-	int result = 0;
-	for (int i = 0; i < N; ++i)
-	{
-		result = max(result, dp(i, things[i].first));
-	}
-	cout << result;
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin >> N >> K;
+    memset(cache, -1, sizeof(cache));
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> W >> V;
+        Objects.push_back(make_pair(W, V));
+    }
+
+    int ret = 0;
+    for (size_t i = 0; i < Objects.size(); ++i)
+    {
+        if (Objects[i].first <= K)
+            ret = max(ret, DP(i, Objects[i].first));
+    }
+    cout << ret;
 }
--- baekjoon - 9251/baekjoon - 9251/baekjoon - 9251.cpp
+++ baekjoon - 9251/baekjoon - 9251/baekjoon - 9251.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <string>
+#include <memory.h>
 using namespace std;
+string A = "", B = "";
+int Answer = 0;
+int cache[1001][1001];
+
+int FindIndexAtB(int start, char target)
+{
+	for (int i = start; i < B.size(); ++i)
+	{
+		if (B[i] == target)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int Dp(int startA, int startB)
+{
+	int& ret = cache[startA][startB];
+	if (ret != -1) return ret;
+	ret = 0;
+	for (int i = startA; i < A.size(); ++i)
+	{
+		int IndexB = FindIndexAtB(startB, A[i]);
+		if (IndexB != -1)
+		{
+			ret = max(ret, Dp(i + 1, IndexB + 1) + 1);
+		}
+	}
+	return ret;
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cin >> A;
+	cin >> B;
+	memset(cache, -1, sizeof(cache));
+
+	int ret = 0;
+	for (int i = 0; i < A.size(); ++i)
+	{
+		ret = max(ret, Dp(i, 0));
+	}
+	cout << ret;
 }
